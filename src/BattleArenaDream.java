dc781f65020a213dd93612c8bda9521999d4d525
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2021-01-13 18:22:58 -0500

    Moved some methods around.

--- src/BattleArenaDream.java
+++ src/BattleArenaDream.java
@@ -6,6 +6,7 @@ public class BattleArenaDream {
 		
 		// variables
 		Scanner scan = new Scanner(System.in);
+		GameMethods methods = new GameMethods();
 		
 		// Intro title
 		System.out.println("***BATTLE ARENA DREAM***\n");
@@ -26,7 +27,7 @@ public class BattleArenaDream {
 			
 			// this loop will go as long as user has not died and they are not out of turns
 			while (turns > 0 && player.isAlive() == true) {
-				showStats(player, enemy); // show player stats
+				methods.showStats(player, enemy); // show player stats
 				
 				// put a pause so the player can read what's happening
 				System.out.println("\n(Hit enter to roll the dice.)");
@@ -36,7 +37,7 @@ public class BattleArenaDream {
 				dice.rollDice();
 				
 				// check the outcome
-				getOutcome(player, enemy, dice, scan);
+				methods.getOutcome(player, enemy, dice, scan);
 				
 				// see if enemy has been defeated
 				
@@ -52,55 +53,7 @@ public class BattleArenaDream {
 
 	}
 	
-	// show player stats
-	public static void showStats(Player player, Enemy enemy) {
-		System.out.println("\n*Stats*");
-		System.out.println("Stamina: " + player.getStamina());
-		System.out.println("Panic level: " + player.getPanic());
-		System.out.println("Enemy HP: " + enemy.getHp());
-	}
 	
-	public static void getOutcome(Player player, Enemy enemy, Dice dice, Scanner scan) {
-		// determine the outcome by what the player rolled
-		int diceRoll = dice.getDiceRoll();
-		switch (diceRoll) {
-		case 1:
-			System.out.println("You're hit!\nYour panic increases by 2.");
-			player.setPanic(player.getPanic() + 2);
-			break;
-		case 2:
-			System.out.println("You dodge the attack!\nStill, you hurt yourself so your stamina " +
-					"decreases by 1.");
-			player.setStamina(player.getStamina() - 1);
-			break;
-		case 3:
-			System.out.println("You deflect the attack!\nNothing happens.");
-			break;
-		case 4:
-			System.out.println("The enemy backs away!\nYour stamina increases by 1.");
-			player.setStamina(player.getStamina() + 1);
-			break;
-		case 5:
-			System.out.println("The enemy dodges, but you strike a glancing blow!" +
-					"\nYour panic reduces by 1. The enemy loses 3 hit points.");
-			player.setPanic(player.getPanic() - 1);
-			enemy.setHp(enemy.getHp() - 3);
-			break;
-		case 6:
-			System.out.println("*WHACK* The enemy takes significant damage!" +
-					"\nYour panic reduces by 3. Your stamina increases by 2. The enemy loses 5 hit points.");
-			player.setPanic(player.getPanic() - 3);
-			player.setStamina(player.getStamina() + 2);
-			enemy.setHp(enemy.getHp() - 5);
-			break;
-		default:
-			System.out.println("ERROR - not a number 1 through 6");
-		}
-		/*
-		System.out.println("\n(Hit enter to continue.)");
-		scan.nextLine();
-		*/
-	}
 	
 
 }
---
+++ src/GameMethods.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class GameMethods {
+
+	// show player stats
+	public void showStats(Player player, Enemy enemy) {
+		System.out.println("\n*Stats*");
+		System.out.println("Stamina: " + player.getStamina());
+		System.out.println("Panic level: " + player.getPanic());
+		System.out.println("Enemy HP: " + enemy.getHp());
+	}
+	
+	public void getOutcome(Player player, Enemy enemy, Dice dice, Scanner scan) {
+		// determine the outcome by what the player rolled
+		int diceRoll = dice.getDiceRoll();
+		switch (diceRoll) {
+		case 1:
+			System.out.println("You're hit!\nYour panic increases by 2.");
+			player.setPanic(player.getPanic() + 2);
+			break;
+		case 2:
+			System.out.println("You dodge the attack!\nStill, you hurt yourself so your stamina " +
+					"decreases by 1.");
+			player.setStamina(player.getStamina() - 1);
+			break;
+		case 3:
+			System.out.println("You deflect the attack!\nNothing happens.");
+			break;
+		case 4:
+			System.out.println("The enemy backs away!\nYour stamina increases by 1.");
+			player.setStamina(player.getStamina() + 1);
+			break;
+		case 5:
+			System.out.println("The enemy dodges, but you strike a glancing blow!" +
+					"\nYour panic reduces by 1. The enemy loses 3 hit points.");
+			player.setPanic(player.getPanic() - 1);
+			enemy.setHp(enemy.getHp() - 3);
+			break;
+		case 6:
+			System.out.println("*WHACK* The enemy takes significant damage!" +
+					"\nYour panic reduces by 3. Your stamina increases by 2. The enemy loses 5 hit points.");
+			player.setPanic(player.getPanic() - 3);
+			player.setStamina(player.getStamina() + 2);
+			enemy.setHp(enemy.getHp() - 5);
+			break;
+		default:
+			System.out.println("ERROR - not a number 1 through 6");
+		}
+		/*
+		System.out.println("\n(Hit enter to continue.)");
+		scan.nextLine();
+		*/
+	}
+}
